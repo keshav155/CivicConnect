98523416c0ac19c11da81fa241e614bfb2f1344e
Kingston Tantanouvong <ktproductions121@gmail.com>
2020-10-01 20:20:54 +1000

    home_screen.dart
    Items required for merging (Still being worked on), Unable to run the application on my behalf possible due to execution faliure.

--- civic_connect/lib/screens/user/home_screen.dart
+++ civic_connect/lib/screens/user/home_screen.dart
@@ -13,7 +13,62 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home Screen"),
+        title: Text('CivicConnect'),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.account_circle),
+          ),
+          Text(
+            'Profile',
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.home),
+          ),
+          Text(
+            'Burwood',
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.school),
+          ),
+          Text(
+            'Education',
+          ),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.history),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.chat),
+              ),
+            ],
+          ),
+        ],
+      ),
+
+      floatingActionButton: FloatingActionButton(
+
+        onPressed: () {},
+        child: Text(
+            '✔',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+            )
+        ),
+        backgroundColor: Colors.greenAccent,
       ),
     );
   }
